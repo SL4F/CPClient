db6ebd79a429a9533da7ffebd778ac9c96465143
SL4F <52598005+SL4F@users.noreply.github.com>
2023-05-20 14:11:26 +1000

    Update constants.ts

--- src/discord/constants.ts
+++ src/discord/constants.ts
@@ -5,11 +5,14 @@ export const SWF_ROOMS_PATH = '/rooms/';
 export const SWF_GAMES_PATH = '/games/';
 export const SWF_QUESTS_GAMES_PATH = '/games/quests/';
 export const SWF_IGLOO_ROOM_PATH = 'igloo.swf';
-export const DISCORD_RPC_CLIENT_APP_ID = '1076298709073145907';
+export const DISCORD_RPC_CLIENT_APP_ID = '1107620907041693747';
 export const LARGE_IMAGE_KEY = 'main-logo';
 export const CPPS_MAP = new Map([
   ['newcp.net', 'New Club Penguin'],
   ['cpbrasil.pw', 'Club Penguin Brasil'],
   ['supercpps.com', 'Super Club Penguin'],
   ['aventurepingouin.com', 'Aventure Pingouin'],
+  ['cplegacy.com', 'Club Penguin Legacy'],
+  ['cpplus.pw', 'Club Penguin Plus'],
+  ['cpchapter2.com', 'Club Penguin Chapter 2'],
 ]);
